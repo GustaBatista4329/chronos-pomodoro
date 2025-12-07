a64f03b10bf1ae93807a220fea2da67a3d370cff
Gustavo Batista <gustavobatista429@gmail.com>
2025-12-07 18:24:09 -0300

    Adicionando o componente de input

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Container } from "./components/Container";
 import { CountDown } from "./components/CountDown";
+import { DefaultInput } from "./components/DefaultInput";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import "./styles/global.css";
@@ -18,6 +19,32 @@ export function App() {
 
       <Container>
         <CountDown />
+      </Container>
+
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <DefaultInput
+              id="meuInput"
+              type="text"
+              labelText="task"
+              placeholder="Digite algo"
+            />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <p>ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className="formRow">
+            <button>enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
