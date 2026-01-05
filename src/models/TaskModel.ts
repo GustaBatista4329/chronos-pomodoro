86a30e052fc6e54f9e1da247af9464c3d33f8299
Gustavo Batista <gustavobatista429@gmail.com>
2026-01-05 11:03:38 -0300

    Adicionando componente GenericHtmal e TaskModel, TaskSate

---
+++ src/models/TaskModel.ts
@@ -0,0 +1,11 @@
+import type { TaskStateModel } from "./TaskStateModel";
+
+export type TaskModel = {
+  id: string;
+  name: string;
+  duration: number;
+  startDate: number;
+  completeDate: number | null; // quando a task chegar ao fim
+  interruptDate: number | null; // quando a task for interrompida
+  type: keyof TaskStateModel["config"];
+};
