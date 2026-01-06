928762122c9f5f37f84421a02f957baadf667edd
Gustavo Batista <gustavobatista429@gmail.com>
2026-01-06 10:33:54 -0300

    Gerencioando estado com Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
+import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 import { Home } from "./pages/Home";
 import "./styles/global.css";
 import "./styles/theme.css";
-import { useState } from "react";
-import type { TaskStateModel } from "./models/TaskStateModel";
-
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formattedSecondsRemaining: "00:00",
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 5,
-    longBreakTime: 15,
-  },
-};
 
 export function App() {
-  const [state, setState] = useState(initialState);
-
-  return <Home />;
+  return (
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
+  );
 }
--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -1,9 +1,10 @@
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import style from "./styles.module.css";
 
 export function CountDown() {
+  const { state } = useTaskContext();
+
   return (
-    <>
-      <div className={style.container}>00:00</div>
-    </>
+    <div className={style.container}>{state.formattedSecondsRemaining}</div>
   );
 }
