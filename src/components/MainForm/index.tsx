9796525876a65024942cfd9c8f5903d3e2bcc94a
Gustavo Batista <gustavobatista429@gmail.com>
2026-01-06 11:08:35 -0300

    Trocando useState para useRef para evitar re-renderização

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,8 +2,11 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
+import { useState, useRef } from "react";
 
 export function MainForm() {
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
   function handleCreateNewTask(
     event: React.MouseEvent<HTMLFormElement, MouseEvent>
   ) {
@@ -19,6 +22,7 @@ export function MainForm() {
           type="text"
           labelText="task"
           placeholder="Digite algo"
+          ref={taskNameInput}
         />
       </div>
 
